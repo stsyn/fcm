59cdf88e2f83dbc1987d8c4456d4ed34566c7894
stsyn <stasyan1902@gmail.com>
2017-02-26 12:57:09 +0500

    build 16

--- core.js
+++ core.js
@@ -15,7 +15,7 @@ function exapi() {
 	this.windows = {};
 	this.mouse = {};
 	this.mouse.onclick = [];
-	this.version = {g:"0.0.1", s:"pre-alpha", b:15};
+	this.version = {g:"0.0.1", s:"pre-alpha", b:16};
 	
 	this.styleSwitch = function(id, variable, change, rewrite, reverse) {
 		if (change) this.settings[variable] = !this.settings[variable];
