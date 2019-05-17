3fe84a92f13a10012a8eaaa6a15e7d2a40aad82b
stsyn <stasyan1902@gmail.com>
2019-05-17 18:01:48 +0500

    Create grayNums.js

---
+++ grayNums.js
@@ -0,0 +1,74 @@
+const grayMath = {};
+grayMath.normalize = (x) => {
+	//выносим знак
+	let minus;
+	if (x[0] < 0) minus = true;
+	else minus = false;
+	
+	//копирование и выравнивание
+	let a = x.map(v => {return Math.abs(parseFloat(v))});
+	a.minus = minus;
+	
+	//копирование и выравнивание
+	if (a.length == 1) return a;
+	if (a[0] > a[1]) [a[0], a[1]] = [a[1], a[0]];
+	return a;
+}
+
+grayMath.denormalize = (x) => {
+	//избавляемся от .minus
+	let a = x.slice();
+	if (x.minus) {
+		if (a.length == 2)  [a[0], a[1]] = [-a[1], -a[0]];
+		else a[0] = -a[0];
+	}
+	return a;
+}
+
+grayMath.add = (a, b) => {
+	//нормализуем и копируем
+	let x = grayMath.normalize(a);
+	let y = grayMath.normalize(b);
+	
+	//а может это вычитание?
+	let inverted = (x.minus != y.minus);
+	
+	//если одно из чисел не серое
+	if (x.length == 1 && y.length == 2) x[1] = x[0];
+	if (x.length == 2 && y.length == 1) y[1] = y[0];
+	
+	//клонируем
+	let z = x.slice();
+	z.minus = x.minus;
+	
+	y.forEach((e, i) => {
+		let index = (inverted ? z.length-i-1 : i);
+		z[index] += e * (inverted?-1:1)
+		if (z[index] < 0) z[index] = 0;
+	});
+	
+	return grayMath.denormalize(z);
+}
+
+grayMath.multiply = (a, b) => {
+	//нормализуем и копируем
+	let x = grayMath.normalize(a);
+	let y = grayMath.normalize(b);
+	
+	let c = [];
+	let z = [];
+	
+	//творим
+	let minus = (x.minus != y.minus);
+	x.forEach(v => {
+		y.forEach(w => { c.push(v*w*(minus?-1:1)) })
+	});
+	
+	if (c.length == 1) z[0] = c[0];
+	else {
+		z[0] = Math.min.apply(Math, c);
+		z[1] = Math.max.apply(Math, c);
+	}
+	
+	return z;
+}
